414d687dcf9a9d682efaa67928e51ec3bafcc182
HonneLi <70879901+HonneLi@users.noreply.github.com>
2022-07-10 20:24:43 +1000

    Update extract.py

--- classifier/extract.py
+++ classifier/extract.py
@@ -11,25 +11,32 @@ from dataLoader import get_dataloaders
 from train_evaluate import extract
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-num_classes = 9
-batch_size = 16
-shuffle_datasets = True
-num_epochs = 50
-save_all_epochs = True
+parser = argparse.ArgumentParser()
+parser.add_argument("--num_classes", help="number of predicted classes",
+                    type=int, default="9")
+parser.add_argument("--video_dir", help="video directory",
+                    type=str, default="../face_video/valid/raw/")
+parser.add_argument("--save_dir", help="saving directory of extracted features",
+                    type=str, default="./valid/full/")
+parser.add_argument("--weight_dir", help="pretrained weight directory",
+                    type=str, default="weights/fullset.pth")
+args = parser.parse_args()
 
-model = ResidualAttentionModel_92(num_classes=num_classes, feat_ext=True)
-#model = model.load_state_dict(torch.load('weights/fullset.pth'))
-model.load_state_dict(torch.load('./weights/nine_raw.pth'))
+num_classes = 9
+
+model = ResidualAttentionModel_92(num_classes=args.num_classes, feat_ext=True)
+#model = model.load_state_dict(torch.load(args.weight_dir))
+model.load_state_dict(torch.load(args.weight_dir))
 model = model.to(device)
 
 print("Extraction progress")
 print("=" * 20)
-set_list = os.listdir('../face_video/valid/raw/')
+set_list = os.listdir(args.video_dir)
 for path in set_list:
     vids = []
-    root = '../face_video/valid/raw/' + path + '/*.mp4'
+    root = args.video_dir + path + '/*.mp4'
     for filename in glob.glob(root):
         vids.append(filename)
-    save_dir = './valid/full/' + path
+    save_dir = args.save_dir + path
     os.makedirs(save_dir, exist_ok=True)
     extract(model=model, dataloader=vids, save_dir=save_dir)
